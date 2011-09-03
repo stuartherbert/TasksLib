00842e64381fd2697ab16dc5844062f5891a3c48
Stuart Herbert <stuart@stuartherbert.com>
2011-09-03 17:43:24 +0100

    Added first tasks, and the TaskQueue to chain them

---
+++ src/php/Phix_Project/TasksLib/Files/RmTask.php
@@ -0,0 +1,125 @@
+<?php
+
+/**
+ * Copyright (c) 2011 Stuart Herbert.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of the copyright holders nor the names of the
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package     Phix_Project
+ * @subpackage  TasksLib
+ * @author      Stuart Herbert <stuart@stuartherbert.com>
+ * @copyright   2011 Stuart Herbert
+ * @license     http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link        http://www.phix-project.org
+ * @version     @@PACKAGE_VERSION@@
+ */
+
+namespace Phix_Project\TasksLib;
+
+class Files_RmdirTask extends TaskBase
+{
+        /**
+         * The folder that we want to remove
+         * 
+         * @var string - path to folder to remove
+         */
+        protected $targetFolder = null;
+        
+        public function initWithFolder($folder)
+        {
+                $target = \realpath($folder);
+                
+                return $this;
+        }
+        
+        protected function requireInitialisedTask()
+        {
+                if ($this->targetFolder == null)
+                {
+                        throw new ETaskNotInitialised(__CLASS__);
+                }
+        }
+        
+        protected function performTask()
+        {
+                // are we removing a file or a folder?
+                if (!is_dir($this->targetFolder))
+                {
+                        // we think we are removing a file
+                        \unlink($this->targetFolder);
+                }
+                else
+                {
+                        // we think we are removing a folder
+                        $this->recursiveRmdir($this->targetFolder);
+                }
+        }
+        
+        protected function recursiveRmdir($folder)
+        {
+                $dir = \opendir($folder);
+                if (!$dir)
+                {
+                        throw new \Exception("unable to open folder " . $folder . ' for reading');
+                }
+
+                while (false !== ($entry = \readdir($dir)))
+                {
+                        if ($entry == '.' || $entry == '..')
+                        {
+                                continue;
+                        }
+
+                        $fqFile = $folder . DIRECTORY_SEPARATOR . $entry;
+                        if (\is_dir($fqFile))
+                        {
+                                $this->recursiveRmdir($fqFile);
+                        }
+                        else
+                        {
+                                \unlink($fqFile);
+                        }
+                }
+
+                \closedir($dir);
+
+                \rmdir($folder);                
+        }
+        
+        protected function requireSuccessfulTask()
+        {
+                // does the folder still exist?
+                if (file_exists($this->targetFolder))
+                {
+                        throw new ETaskFailed(__CLASS__, $this->targetFolder . " exists after removal attempt");
+                }
+        }
+}
